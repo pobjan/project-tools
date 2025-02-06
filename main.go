2a41394c98f703e414338923faf10ee464b55986
po <113539925+pobjan@users.noreply.github.com>
2025-02-06 19:56:58 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
+// main.go
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "image.jpg")  // Zakładając, że masz plik 'image.jpg' w tym samym katalogu
-}
-
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Serwer uruchomiony na porcie 8080...")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		file, err := os.Open("./image.jpg")
+		if err != nil {
+			http.Error(w, "File not found", http.StatusNotFound)
+			return
+		}
+		defer file.Close()
+
+		// Pobranie informacji o pliku
+		info, err := file.Stat()
+		if err != nil {
+			http.Error(w, "Could not get file info", http.StatusInternalServerError)
+			return
+		}
+
+		// Ustawienie nagłówków
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", info.Size()))
+		w.Header().Set("Content-Type", "image/jpeg")
+		w.Header().Set("Date", time.Now().Format(http.TimeFormat))
+
+		// Wysłanie pliku jako odpowiedź
+		http.ServeFile(w, r, "./image.jpg")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
